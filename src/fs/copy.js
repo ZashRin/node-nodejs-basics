32dca5316956950e950bd804a7bab27e9505d336
Alex <itgtktdbx@gmail.com>
2022-05-31 16:53:25 +0300

    feat: add copy task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./');
+
 export const copy = async () => {
-    // Write your code here 
+    if (!fs.existsSync('./files')) {
+        throw new Error('FS operation failed');
+    }
+    fs.mkdir(
+        path.join(__dirname, 'files_copy'), 
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    );
+    fs.cp(
+        path.join(__dirname, 'files'), 
+        path.join(__dirname, 'files_copy'), 
+        { recursive: true }, 
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    );
 };
+
+copy();
