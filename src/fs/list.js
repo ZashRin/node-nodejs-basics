600c98d572c1a7c6231331795bc97b329a2104af
Alex <itgtktdbx@gmail.com>
2022-05-31 17:39:49 +0300

    feat: add list task solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./');
+
 export const list = async () => {
-    // Write your code here 
+    fs.readdir(
+        path.join(__dirname, 'files'), 
+        (err, files) => {
+            if(err) throw new Error('FS operation failed');
+            files.forEach(file => console.log(file))
+        }
+    );
 };
+
+list();
