7367adb8b768349d091d45ffb93fbf4a2a3edf7c
Alex <itgtktdbx@gmail.com>
2022-05-31 17:21:58 +0300

    feat: add rename task solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname('./');
+
 export const rename = async () => {
-    // Write your code here 
+    if(fs.existsSync(path.join(__dirname, 'files/properFilename.md'))) {
+        throw new Error('FS operation failed');
+    }
+    fs.rename(
+        path.join(__dirname, 'files/wrongFilename.txt'),
+        path.join(__dirname, 'files/properFilename.md'),  
+        (err) => {
+            if(err) throw new Error('FS operation failed');
+        }
+    );
 };
+
+rename();
