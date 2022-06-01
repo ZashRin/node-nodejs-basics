0459f3e8da05da0f3017db05fc1cbde03d0ac291
Alex <itgtktdbx@gmail.com>
2022-06-01 12:54:44 +0300

    feat: add streams tasks  solutions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdin } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+    const stream = fs.createWriteStream(path.join(__dirname, 'files/fileToWrite.txt'));
+    stdin.pipe(stream);
 };
+
+write();
