39d773225b7d5c631701c8a83d8ef05f0a9e086e
Alex <itgtktdbx@gmail.com>
2022-06-01 10:30:27 +0300

    feat: add cli tasks solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.map((_, i) => {
+            if (i % 2) return;
+            console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+        });
 };
+
+parseArgs();
